35c8e36bd7e19a4c3e0e71d9631727c938682ad2
victorkaplunov <victorkaplunov@yandex.ru>
2022-11-12 16:36:07 +0300

    add tabs.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+
 st.set_page_config(
    page_title="Streamlit test app",
    page_icon="🧊",
@@ -8,3 +9,20 @@ st.set_page_config(
 )
 
 st.title('Streamlit test app')
+
+dict_ = "<h1>HTML repr</h1>"
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+
+with tab1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+
+with tab2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+
+st.write(dict_, unsafe_allow_html=True)
