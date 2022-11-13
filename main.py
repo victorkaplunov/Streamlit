a26aa21ce02910c9ae348e87352031af13a5e502
victorkaplunov <victorkaplunov@yandex.ru>
2022-11-13 20:34:35 +0300

    add titles translation.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import streamlit as st
+import json
 
+import streamlit as st
+from streamlit_echarts import st_echarts
 
 st.set_page_config(
    page_title="Streamlit test app",
@@ -15,7 +17,59 @@ tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
 
 with tab1:
    st.header("A cat")
-   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+
+   from pyecharts import options as opts
+   from pyecharts.charts import Bar
+   from streamlit_echarts import st_pyecharts
+
+   b = (
+      Bar()
+      .add_xaxis(["Microsoft", "Amazon", "IBM", "Oracle", "Google", "Alibaba"])
+      .add_yaxis(
+         "2017-2018 Revenue in (billion $)", [21.2, 20.4, 10.3, 6.08, 4, 2.2]
+      )
+      .set_global_opts(
+         title_opts=opts.TitleOpts(
+            title="Top cloud providers 2018", subtitle="2017-2018 Revenue"
+         ),
+         toolbox_opts=opts.ToolboxOpts(
+            feature=opts.ToolBoxFeatureOpts(
+               brush=opts.ToolBoxFeatureBrushOpts(
+                  rect_title='Прямоугольник',
+                  polygon_title='Полигон',
+                  line_x_title='Ось X',
+                  line_y_title='Ось Y',
+                  clear_title='Очистить заголовок',
+                  keep_title='Сохранить заголовок'
+               ),
+               save_as_image=opts.ToolBoxFeatureSaveAsImageOpts(
+                  title='Сохранит как изображение'
+                  ),
+               restore=opts.ToolBoxFeatureRestoreOpts(
+                  title='Восстановить'
+                  ),
+               data_view=opts.ToolBoxFeatureDataViewOpts(
+                  title='Показать данные',
+                  lang=["Данные графика", "Отказаться", "Сохранить"]
+                  ),
+               data_zoom=opts.ToolBoxFeatureDataZoomOpts(
+                  zoom_title='Увеличить',
+                  back_title='Уменьшить'
+                  ),
+               magic_type=opts.ToolBoxFeatureMagicTypeOpts(
+                  line_title='Линейный',
+                  bar_title='Столбчатый',
+                  stack_title='Стопка',
+                  tiled_title='Плитка'
+
+
+               )
+
+            )
+         ),
+      )
+   )
+   st_pyecharts(b)
 
 with tab2:
    st.header("A dog")
